78af44bd18dc0b92d0f1b061ae71d210ae4ebda0
Stian M Tellefsen <89570393+s366449@users.noreply.github.com>
2022-02-15 08:38:24 +0100

    Add files via upload

---
+++ Oblig1Data1700b/src/main/java/stiansoblig/oblig1data1700b/Oblig1Data1700bApplication.java
@@ -0,0 +1,13 @@
+package stiansoblig.oblig1data1700b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oblig1Data1700bApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oblig1Data1700bApplication.class, args);
+    }
+
+}
---
+++ Oblig1Data1700b/src/test/java/stiansoblig/oblig1data1700b/Oblig1Data1700bApplicationTests.java
@@ -0,0 +1,13 @@
+package stiansoblig.oblig1data1700b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig1Data1700bApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
